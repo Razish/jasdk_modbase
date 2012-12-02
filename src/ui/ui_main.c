8c38088446bd2bd08b75621172ca3049bc37ad38
Razish <mrrazish@gmail.com>
2012-12-02 20:22:36 +1100

    Download message percentage overflow

--- src/ui/ui_main.c
+++ src/ui/ui_main.c
@@ -10697,7 +10697,7 @@ static void UI_DisplayDownloadInfo( const char *downloadName, float centerPoint,
 	Text_PaintCenter(centerPoint, yStart + 248, scale, colorWhite, sTransferRate, 0, iMenuFont);
 
 	if (downloadSize > 0) {
-		s = va( "%s (%d%%)", downloadName, downloadCount * 100 / downloadSize );
+		s = va( "%s (%d%%)", downloadName, (int)( (float)downloadCount * 100.0f / downloadSize ) );
 	} else {
 		s = downloadName;
 	}
